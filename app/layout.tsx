34fa70c1d6f9ebb05f224fb1c9bc71c2c8805f98
sixmillions <liubw95@163.com>
2023-11-27 00:45:28 +0000

    ESLint Prettier Config

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,27 +1,31 @@
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import './globals.css'
+import React from "next";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
 import ThemeProvider from "@/context/ThemeProvider";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'Next Blog',
-  description: 'My Dev Blog',
-}
+  title: "Next Blog",
+  description: "My Dev Blog",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="zh-CN" suppressHydrationWarning>
       <body className={inter.className}>
-        <ThemeProvider value={{ light: 'garden', dark: 'night' }} defaultTheme="dark">
+        <ThemeProvider
+          value={{ light: "garden", dark: "night" }}
+          defaultTheme="dark"
+        >
           {children}
         </ThemeProvider>
       </body>
     </html>
-  )
+  );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,14 +1,14 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   plugins: [require("daisyui")],
   daisyui: {
-    themes: ["garden", "synthwave",  "night"],
+    themes: ["garden", "synthwave", "night"],
   },
-}
-export default config
+};
+export default config;
